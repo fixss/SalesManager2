8652210cd094712fdded611065805a1b214439b7
fixss <38314679+fixss@users.noreply.github.com>
2022-08-21 23:47:14 +0600

    Added getMinValue and getAverage

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        int[] saleManagerArray = {1, 5, 19};
+        int[] saleManagerArray = {2, 5, 19, 25, 55, 99,};
         SalesManager salesManager = new SalesManager(saleManagerArray);
 
         System.out.println(salesManager.max());
+        System.out.println(salesManager.getMinValue());
+        System.out.println(salesManager.getAverage());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,24 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int getMinValue(){
+        int minValue = sales[0];
+        for(int i=1; i<sales.length; i++){
+            if(sales[i] < minValue){
+                minValue = sales[i];
+            }
+        }
+        return minValue;
+    }
+    public double getAverage() {
+        int sum = 0;
+        double average;
+        for(int i=0; i < sales.length; i++){
+            sum = sum + sales[i];
+        }
+        average = ((double)sum - (max() + getMinValue())) / (sales.length -2);
+        return average;
+    }
 }
+
