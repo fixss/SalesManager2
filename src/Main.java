b174f8fa33e5a287407aaf7826b24c8f3bc10652
fixss <38314679+fixss@users.noreply.github.com>
2022-08-21 23:02:06 +0600

    Changed int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        int[] saleManagerArray = {1, 5, 19};
+        long[] saleManagerArray = {1, 5, 19};
         SalesManager salesManager = new SalesManager(saleManagerArray);
 
         System.out.println(salesManager.max());
